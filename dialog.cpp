3458a49c2a745d6aa343d3000f280ced96f7ba09
nishu65 <nishubhaiqwerty@gmail.com>
2025-02-04 09:03:11 +0530

    stylesheet apply

--- dialog.cpp
+++ dialog.cpp
@@ -7,7 +7,7 @@ Dialog::Dialog(QWidget *parent)
     , ui(new Ui::Dialog)
 {
     ui->setupUi(this);
-    QPixmap pix("C:/Users/nishu/OneDrive/Pictures/v.png");
+    QPixmap pix(":/resrc/PICC/v.png");
     int w= ui->label_pic2->width() ;
     int h=ui->label_pic2->height();
     ui->label_pic2->setPixmap(pix.scaled( w,h,Qt::KeepAspectRatio));
